046bf9940209abc74df7f9ce19a3cc6d00023163
Vishnu Kumar <vermaji1891@gmail.com>
2024-06-04 11:43:08 +0530

    added basic setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+
+const App = () => {
+  return (
+    <h1>You App Conmpone</h1>
+  )
+}
+
+export default App;
---
+++ src/components/Demo/Demo.jsx
@@ -0,0 +1,9 @@
+const Demo = () => {
+    return (
+        <>
+            <h1>You are demo page</h1>
+        </>
+    )
+}
+
+export default Demo;
